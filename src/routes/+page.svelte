fc9401a6800b01c1bbf0711fce4bad44889972bf
Josh Buchanan <48423110+jbuchanan00@users.noreply.github.com>
2025-05-11 22:06:55 -0600

    added edit page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,10 +9,24 @@
         <ProfileImageCard />
         <ProfileInfoCard/>
     </div>
+    <div class="diagonalElement"></div>
     <div class="bodySection">
+        
         <div class="postCard">
             <img src={photo} alt="test" />
             <p>Text</p>
+        </div>
+        <div class="postCard">
+            <img src={photo} alt="test" />
+            <p>Goes</p>
+        </div>
+        <div class="postCard">
+            <img src={photo} alt="test" />
+            <p>In</p>
+        </div>
+        <div class="postCard">
+            <img src={photo} alt="test" />
+            <p>Here</p>
         </div>
         <div class="postCard">
             <img src={photo} alt="test" />
@@ -30,11 +44,23 @@
 </div>
 
 <style>
+    .diagonalElement {
+        border: 2px solid #000;
+        transform: rotate(16deg) translate(-40%, 100px);
+        margin-top: 50px;
+        margin-right: 150px;
+        z-index: -100;
+        width: 250%;
+        position: absolute;
+        background-color: #ffb2ef;
+        height: 100%;
+    }
     p {
         font-weight: lighter;
         font-size: .75em;
     }
     .bodySection {
+        z-index: 1;
         margin-top: 50px;
         display: flex;
         flex-wrap: wrap;
@@ -48,6 +74,7 @@
         padding: 0px 20px;
     }
     .postCard {
+        z-index: 1;
         display: flex;
         flex-direction: column;
         width: 28%;
