cfdfdee6df1dfc03506b628669cab24ae63efd5f
Josh <josh.buchanan00@gmail.com>
2025-07-05 12:36:35 -0600

    working on jwts

--- polvo-docker-db/init.sql
+++ polvo-docker-db/init.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS role (
 );
 
 CREATE TABLE IF NOT EXISTS account (
-    id BIGSERIAL PRIMARY KEY,
+    id varchar PRIMARY KEY,
     firstName Text,
     lastName Text,
     email Text,
@@ -19,6 +19,22 @@ CREATE TABLE IF NOT EXISTS authentication (
     userId bigint PRIMARY KEY REFERENCES account(id),
     hash Text,
     salt Text
+);
+
+CREATE TABLE IF NOT EXISTS loginMethod (
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR
+)
+
+CREATE TABLE IF NOT EXISTS login (
+    userId VARCHAR REFERENCES account(id),
+    loginMethod BIGINT REFERENCES loginMethod(id),
+    lastLogin TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS SESSION (
+    id VARCHAR PRIMARY KEY,
+    
 );
 
 INSERT INTO role (rolename) VALUES ('canvas'), ('artist'), ('shop');
