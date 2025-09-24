06db88335a8b2de0d0c83bba18584946d6618608
Josh <josh.buchanan00@gmail.com>
2025-09-24 17:26:46 -0600

    adding the fetching of instagram posts

--- src/routes/[slug]/instagramposts/+server.ts
+++ src/routes/[slug]/instagramposts/+server.ts
@@ -1,8 +1,43 @@
+import { decrypt } from "$lib/server/api/helpers";
+import { getLLToken } from "$lib/server/api/tokens";
 import type { RequestHandler } from "@sveltejs/kit";
 
 
-export const GET: RequestHandler = ({params, request}) => {
+export const GET: RequestHandler = async ({params, request, locals}) => {
     const userId = params.slug
+    const apiKey = request.headers.get('x-api-key')
+
+    if(!userId){
+        return Response.error()
+    }
+
+    try{
+        const pool = await locals.db()
+        const {token, iv, tag, provider_user_id: instaId} = await getLLToken(pool, userId)
+        pool.release()
+
+        const accessToken = decrypt(token, iv, tag)
+
+        const res = await fetch(`${process.env.INSTAGRAM_GRAPH_BASE}/${instaId}/media?access_token=${accessToken}`)
+
+        if(!res.ok){
+            console.log('Error retrieving posts from instagram')
+            return Response.error()
+        }
+
+        const {data} = await res.json()
+
+        if(!data){
+            console.log('Error with getting data from response')
+            return Response.error()
+        }
+
+        
+
+    }catch(e){
+        console.log('Error gathering instagram posts', e)
+        return Response.error()
+    }
 
     return new Response()
 }
