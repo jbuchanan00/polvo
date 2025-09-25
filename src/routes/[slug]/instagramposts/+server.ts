c4b5c5a974bec3cb346ebebb983e24d538acc196
Josh Buchanan <josh.buchanan00@gmail.com>
2025-09-24 20:54:29 -0600

    added getting cursor for after in instagram call and return info

--- src/routes/[slug]/instagramposts/+server.ts
+++ src/routes/[slug]/instagramposts/+server.ts
@@ -6,7 +6,7 @@ import type { RequestHandler } from "@sveltejs/kit";
 export const POST: RequestHandler = async ({params, request, locals}) => {
     const userId = params.slug
     const apiKey = request.headers.get('x-api-key')
-    const {postIds: existingPostIds} = await request.json()
+    const {postIds: existingPostIds, after} = await request.json()
 
     if(!userId){
         return Response.error()
@@ -19,26 +19,34 @@ export const POST: RequestHandler = async ({params, request, locals}) => {
 
         const accessToken = decrypt(token, iv, tag)
 
-        const res = await fetch(`${process.env.INSTAGRAM_GRAPH_BASE}/${instaId}/media?access_token=${accessToken}`)
+        const res = await fetch(`${process.env.INSTAGRAM_GRAPH_BASE}/${instaId}/media?access_token=${accessToken}&after=${after}`)
 
         if(!res.ok){
             console.log('Error retrieving posts from instagram')
             return Response.error()
         }
 
-        const {data} = await res.json()
+        const {data, paging} = await res.json()
 
-        if(!data){
+        if(!data || !paging){
             console.log('Error with getting data from response')
             return Response.error()
         }
 
+        let idsToCall: Promise<Response>[] = []
 
+        data.array.forEach((element: {id: string}) => {
+            idsToCall.push(fetch(`${process.env.INSTAGRAM_GRAPH_BASE}/${element.id}/fields=id,caption,media_type,media_url,permalink,timestamp`))
+        });
+
+        const postsData = await Promise.all(idsToCall)
+
+        const postsDataJson = postsData.map(async element => await element.json())
+
+        return new Response(JSON.stringify({posts: postsDataJson, cursor: paging.cursors.after}))
 
     }catch(e){
         console.log('Error gathering instagram posts', e)
         return Response.error()
     }
-
-    return new Response()
 }
