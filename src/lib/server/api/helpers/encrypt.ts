981250dd06ee40fb97e7917d2f2ad60cf7830071
Josh Buchanan <josh.buchanan00@gmail.com>
2025-09-27 20:21:45 -0600

    added adhoc so I could do some local testing without getting meta tokens from the vps tunnel

--- src/lib/server/api/helpers/encrypt.ts
+++ src/lib/server/api/helpers/encrypt.ts
@@ -6,7 +6,7 @@ dotenv.config()
 export function encrypt(token: string){
     const iv = crypto.randomBytes(12).toString('base64')
 
-    const cipher = crypto.createCipheriv('aes-128-gcm', process.env.ACCESS_TOKEN_SECRET!, iv)
+    const cipher = crypto.createCipheriv('aes-128-gcm', Buffer.from(process.env.ACCESS_TOKEN_SECRET!, 'hex'), iv)
 
     let ciphertext = cipher.update(token, 'utf8', 'base64');
  
