c2474941b9e98de3a351e320252148904348fff0
Josh Buchanan <josh.buchanan00@gmail.com>
2025-11-15 22:51:07 -0700

    working on getting instagram authorization redirect to be accepted. Changed origin in meta configuration.

--- src/routes/auth/meta/+server.ts
+++ src/routes/auth/meta/+server.ts
@@ -1,6 +1,5 @@
 import dotenv from 'dotenv'
 import { redirect, type RequestHandler } from '@sveltejs/kit'
-import { resolve } from '$app/paths'
 import { getRedis } from '$lib/server/redis'
 import {randomBytes} from 'node:crypto'
 
@@ -15,10 +14,11 @@ export const GET: RequestHandler = async ({locals}) => {
     await r.set(`ig:state:${state}`, JSON.stringify({userId, state}), {EX: 600})
 
     const rawParams = {
+            force_reauth: 'true',
             client_id: process.env.INSTAGRAM_CLIENT_ID!,
-            redirect_uri: `https://api.inked-out.com/auth/instagram/callback`,
+            redirect_uri: `https://app.inked-out.com/profile/auth/meta/callback`,
             response_type: 'code',
-            scope: 'instagram_business_content_publish,instagram_business_basic',
+            scope: 'instagram_business_basic,instagram_business_manage_messages,instagram_business_manage_comments,instagram_business_content_publish,instagram_business_manage_insights',
             state
         }
 
