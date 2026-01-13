432f542afcd24e8dd9a86c1ac6f6a347b4250d54
Josh Buchanan <josh.buchanan00@gmail.com>
2026-01-12 18:54:07 -0700

    formatting users to use in db call

--- src/routes/users/location/+server.ts
+++ src/routes/users/location/+server.ts
@@ -5,21 +5,28 @@ import type { RequestHandler } from "@sveltejs/kit"
 export const GET: RequestHandler = async ({url, fetch, locals}) => {
     const locations = url.searchParams.getAll('loc')
 
-    if(!locations){
+    if(!locations || locations.length < 1){
         return new Response('No locations in url')
     }
 
     console.log('locations:', locations)
 
+    const formattedLocations = locations.map(loc => {
+        const arr = loc.split(',')
+        return {lat: parseInt(arr[0]), lng: parseInt(arr[1])}
+    })
+
     const pool = await locals.db()
 
     try{
-        // let usersInLocations = await getUsersByLocations(pool, locations)
+        let usersInLocations = await getUsersByLocations(pool, formattedLocations)
+        pool.release()
+        return new Response(JSON.stringify(usersInLocations))
     }catch(e){
-
+        console.log('Error getting users in locations:', e)
+        pool.release()
+        return new Response('Error in fetching')
     }
-
-    return new Response()
 }
 
 export const POST = () => {
