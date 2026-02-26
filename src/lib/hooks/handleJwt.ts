0fff707068b3077334218072bee75fc23ec0ca13
jbuchanan <josh.buchanan00@gmail.com>
2026-02-26 16:53:32 -0700

    added a little check for dev env to test instagram getting

--- src/lib/hooks/handleJwt.ts
+++ src/lib/hooks/handleJwt.ts
@@ -10,14 +10,16 @@ export const handleJwt: Handle = async ({event, resolve}) => {
         if(userJwt?.user_id){
             event.locals.user = {id: userJwt.user_id}
         }else{
+            if(process.env.ENVIORNMENT === "dev" && !token){
+                event.locals.user = {id: '11111111-1111-1111-1111-111111111111'}
+                console.log('ENV is dev: adding id to locals, check->', event.locals)
+            }
             console.log("Could'nt find user_id on the token")
         }
     }else{
         console.log('No Token')
     }
-    if(process.env.ENVIORNMENT === "dev" && !token){
-        event.locals.user = {id: '11111111-1111-1111-1111-111111111111'}
-    }
+
     
 
     return resolve(event)
