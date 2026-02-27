a986b7564e3adb34f0093a43a982558b16c3a6a8
jbuchanan <josh.buchanan00@gmail.com>
2026-02-26 17:08:10 -0700

    added some logs and more places to add a user id in dev

--- src/lib/hooks/handleJwt.ts
+++ src/lib/hooks/handleJwt.ts
@@ -10,13 +10,17 @@ export const handleJwt: Handle = async ({event, resolve}) => {
         if(userJwt?.user_id){
             event.locals.user = {id: userJwt.user_id}
         }else{
-            if(process.env.ENVIORNMENT === "dev" && !token){
+            if(process.env.ENVIORNMENT === "dev"){
                 event.locals.user = {id: '11111111-1111-1111-1111-111111111111'}
                 console.log('ENV is dev: adding id to locals, check->', event.locals)
             }
             console.log("Could'nt find user_id on the token")
         }
     }else{
+        if(process.env.ENVIORNMENT === "dev"){
+            event.locals.user = {id: '11111111-1111-1111-1111-111111111111'}
+            console.log('ENV is dev: adding id to locals, check->', event.locals)
+        }
         console.log('No Token')
     }
 
--- src/routes/auth/meta/+server.ts
+++ src/routes/auth/meta/+server.ts
@@ -7,6 +7,7 @@ dotenv.config()
 
 export const GET: RequestHandler = async ({locals}) => {
     const userId = locals.user.id
+    console.log("Locals:", locals)
     const state = randomBytes(16).toString('hex')
     const r = getRedis()
     await r.set(`ig:state:${state}`, JSON.stringify({userId, state}), {EX: 600})
