e9da7389be63caf4a4f1afa5eb61662fc0ebbe72
Josh <josh.buchanan00@gmail.com>
2026-06-23 19:51:16 -0600

    switched around meta auth to accept user id instead of looking in locals

--- src/routes/auth/meta/+server.ts
+++ src/routes/auth/meta/+server.ts
@@ -5,15 +5,8 @@ import {randomBytes} from 'node:crypto'
 
 dotenv.config()
 
-export const GET: RequestHandler = async ({locals}) => {
-    console.log("Environment:", process.env.ENVIRONMENT)
-    let userId
-    if(process.env.ENVIRONMENT === 'dev'){
-        userId = '11111111-1111-1111-1111-111111111111'
-    }else{
-        userId = locals.user.id
-    }
-    console.log("Locals:", locals)
+export const GET: RequestHandler = async ({url}) => {
+    const userId = url.searchParams.get("id");
     const state = randomBytes(16).toString('hex')
     const r = getRedis()
     await r.set(`ig:state:${state}`, JSON.stringify({userId, state}), {EX: 600})
