f667190149d9cc029538e4c063a8c544fc5a5758
admin <admin@admins-MacBook-Pro.local>
2026-07-14 20:05:40 -0600

    added a check for the google oauth since it will be different in dev than prod.

--- src/lib/db/db.ts
+++ src/lib/db/db.ts
@@ -1,10 +1,8 @@
 import pkg from 'pg'
-const {Pool} = pkg
+const { Pool } = pkg
 import dotenv from 'dotenv'
 
 dotenv.config()
-
-  console.log(process.env.POSTGRES_USER_POLVO, process.env.POSTGRES_HOST_POLVO, process.env.POSTGRES_DB_POLVO, process.env.POSTGRES_PASSWORD_POLVO, process.env.POSTGRES_PORT_POLVO)
 
 const pool = new Pool({
   user: process.env.POSTGRES_USER_POLVO,
